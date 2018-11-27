d4be20a1d1a8774003ff27caa91bdf53c4ba4c36
Eduardo D. Romero Hurtado <eduardodrh@gmail.com>
2018-11-27 17:27:39 -0600

    Crea las poblaciones iniciales
    El programa genera varias tuplas de numeros ente-
    ros los cuales serviran de poblaciÃn para el al-
    goritmo genÃtico.

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-
+from random import randint
 
 def main():
+	# Aun estoy construllendo las funciones
 	print("Y así iniciamos")
+	crear_poblacion(16,10)
+
+
+def crear_poblacion(medida, elementos):
+	
+	poblacion = []
+	inicio = 0
+
+	for x in range(inicio,medida):
+		poblacion.append([])
+		for y in range(inicio,elementos):
+			poblacion[x].append(randint(inicio,elementos))
+
+	for pob in range(inicio,10):
+		print(poblacion[pob])
+
+
+
+
 
 main()
